0f457c373856551b6c52476cb5db1e87bf9e40ae
Chris Nicolls <chris@devtree.io>
2017-11-03 16:14:29 -0700

    HeroList, VillainList, Router, VillainDetails, HeroDetails, All API Files

--- super-client/src/views/ListVillains.js
+++ super-client/src/views/ListVillains.js
@@ -1,18 +1,48 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import api from "../api";
 
-class Home extends Component {
+class ListVillains extends Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      villains: []
+    };
+  }
+
+  componentDidMount() {
+    api.villains.getAll().then(villains => {
+      if (!villains.length && villains.legth !== 0) {
+        console.log("return value was not an array of Villains", villains);
+        villains = [];
+
+        this.setState(state => {
+          return {
+            villains: villains
+          };
+        });
+      }
+    });
   }
 
   render() {
+    let { villains } = this.state;
     return (
       <div>
-        <h1>HOME</h1>
+        <h1>Villains List</h1>
+        <Link style={{ float: "right" }} to={`/villains/new`}>
+          New Villain
+        </Link>
+        {villains.map(v => (
+          <div style={{ padding: 10 }} key={v.id}>
+            <Link to={`/villains/${v.id}`}>
+              {v.name} - {v.villains.length} Villains
+            </Link>
+          </div>
+        ))}
       </div>
     );
   }
 }
-export default Home;
+export default ListVillains;
